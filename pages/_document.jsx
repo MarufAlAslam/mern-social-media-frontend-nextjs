b665e47d84e9ab51e38e5cdba6e2dd1774ef76e1
marufAlAslam <maruf14@cse.pstu.ac.bd>
2023-09-24 11:39:08 +0600

    meta tags added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,27 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="https://cdn-icons-png.flaticon.com/512/4336/4336400.png" />
+          <link
+            rel="icon"
+            href="https://cdn-icons-png.flaticon.com/512/4336/4336400.png"
+          />
+          <meta
+            name="description"
+            content="A social media app built with the MERN stack and TailwindCSS"
+            key="desc"
+          />
+          <meta
+            property="og:title"
+            content="A social media app built with the MERN stack and TailwindCSS"
+          />
+          <meta
+            property="og:description"
+            content="A social media app built with the MERN stack and TailwindCSS"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
         </Head>
         <body>
           <Main />
